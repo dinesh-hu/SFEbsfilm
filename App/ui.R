7747ced1a0ef9ffc8b3c9a462b182f1a4fe25f0c
dinesh-hu <dinesh-91@hotmail.fr>
2016-12-04 23:44:35 +0100

    Update ui.R

--- App/ui.R
+++ App/ui.R
@@ -1,40 +1,44 @@
 library(plotly)
 library(shiny)
 
+fluidPage( 
+  
 
-fluidPage(    
   
-  # Give the page a title
-  titlePanel("Black–Scholes Formula for European Options"),
+  titlePanel("Black-Scholes formula for European call price"),
   
-  # Generate a row with a sidebar
   sidebarLayout(      
     
-    # Define the sidebar with one input
     sidebarPanel(
-      numericInput("S", "Stock price:",110),
+      
       numericInput("K", "Strike price:",100),
       sliderInput("r",
                   "Risk free interest rate:",
                   min = 0,  max = 1, value = 0.1,step = 0.1),
-      numericInput("sigma", "Volatility:",0.3),
+      numericInput("sigma", "Volatility:",0.3,step = 0.1),
     
-      sliderInput("tau",
-                "Time :",
-                min = 0,  max = 1, value = 0.1,animate=TRUE,step = 0.1)
+      sliderInput("t",
+                "Time t (t=c-τ):",
+                min = 0,  max = 1, value = 0.1,animate=TRUE,step = 0.05),
+      
+      hr()
     ),
 
     
+    
     mainPanel(
       
-      plotlyOutput("Plot_BS")
-       
+                tabPanel("Call", plotlyOutput("Plot_BS_Call"))
+                  
     )
     
+      
+      
+       
   )
-
-  ###  
+  
+  
+  
+  
+  
 )
-
-
-
